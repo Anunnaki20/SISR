b63cc3df3eddddded4bf4fac213c07fbcac91299
Matthew <mji544@usask.ca>
2022-02-21 13:25:39 -0600

    Fixed receiving image and parameters.

--- SISR.py
+++ SISR.py
@@ -1,14 +1,14 @@
 # Web Framework
+from email.mime import base
 from flask import Flask
 from flask import request, redirect, Response
 from flask import jsonify
 
-import json
 import cv2
 import os
-import io
 import PIL.Image as Image
-import base64
+import zipfile
+import shutil
 
 # Tensorflow libraries
 # import tensorflow as tf
@@ -33,99 +33,68 @@ def test():
     if request.method == 'POST':
 
         r = request
-        # info = request.get_json()
-        # print(jsonify(info.model))
-        # print(json.loads(info)["model"])
-        # print(r.json()["model"])
-        # print(r.data["model"])
-        # print(r.data["scaleAmount"])
-        # data = json.loads(r.data)
-        # print(r.json)
-        # stuff = json.loads(r.text)
-        # print(stuff["scaleAmount"])
 
-        # print(str(r.data))
+        parameters = r.args
+        # print(r.args)
+        filetype = parameters['type']
+        scale = parameters['scaleAmount']
+        model = parameters['model']
+        qualityMeasure = parameters['qualityMeasure']
+        print("File Type:", filetype, ", Scale:", scale, ", Model:", model, ", Quality Measure?:", qualityMeasure)
 
-        scale = r.json['scaleAmount']
-        model = r.json['model']
-        print("Scale:", scale, ", Model:", model)
-        # data = r.data['image']
+        if filetype == "zip":
+            zip_result = open('./uploadedFile/testzip.zip', 'wb')
+            zip_result.write(r.data)
 
-        # nparr = np.frombuffer(arr, np.uint8)
-        # img = base64.b64decode(arr)
-        # nparr = np.frombuffer(img, np.uint8)
-        # nparr = np.asarray(arr)
-        
-        # data = base64.b64decode(arr)
-        # print(str(scale), str(model))
-        # print(data["model"])
-        # print()
-        # print(data["scaleAmount"])
-        nparr = np.frombuffer(r.data, np.uint8)
+            ######################################################
+            # unzip the file and check image size for each image #
+            ######################################################
+            file_url = "/uploadedFile/testzip.zip"
+            # extract the images from the zip
+            with zipfile.ZipFile("."+file_url, 'r') as zip_ref:
+                zip_ref.extractall("./uploadedFile/extractedImages")
 
-        # convert string of image data to uint8
-        # nparr = np.frombuffer(r.data, np.uint8)
-        # print(r.data)
-        # print()
-        # print(r.headers)
-        # decode image
-        img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE) #IMREAD_GRAYSCALE #IMREAD_COLOR
-        # Now we can do whatever we want with this decoded image ...
+            ####################################
+            # Upscale each image in the folder #
+            ####################################
 
-        # new_img = cv2.imread("testimage.png", cv2.IMREAD_GRAYSCALE)
-        # # Displaying the image
-        # cv2.imshow('image', arr)
 
-        ############## Save the image as a png ##############
-        # cv2.imwrite("testimage.png", img)
-        
-
-        # This code here is to send back to the "client" the front-end to show that it received the image and reads it properly.
-        # build a response dict to send back to client
-        response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+        else: #filetype == "single_image"
+            # convert string of image data to uint8
+            nparr = np.frombuffer(r.data, np.uint8)
+            # decode image
+            img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE) #IMREAD_GRAYSCALE #IMREAD_COLOR
+            
+            response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
                     }
-        print(response)
-        # encode response using jsonpickle
-        # response_pickled = jsonpickle.encode(response)
+            print(response)
 
-        # return Response(response=response_pickled, status=200, mimetype="application/json")
+            ############## Save the image as a png ##############
+            cv2.imwrite("./uploadedFile/decodedimage.png", img)
+
+            ###################################
+            # Upscale the image in the folder #
+            ###################################
+
+
+        ########################
+        # Zip the single image #
+        ########################
+        shutil.make_archive("./upscaledImages/upscaled", 'zip', "./uploadedFile/extractedImages")
+        file_url = "/upscaledImages/upscaled.zip"
+
+        ################################
+        # Send the zip back to website #
+        ################################
+
+        ##########################
+        # Delete all saved files #
+        ##########################
+        cleanDirectories()
+
+        # This is just a dummy variable
         data = "Nothing"
 
-
-        ######################################################################################################################################################
-        ######################################################################################################################################################
-        ############### The following code was used for testing. Keep this here just in case we need to send the image or files in this way ##################
-        ######################################################################################################################################################
-        ######################################################################################################################################################
-        # print(request.files)
-        # data = request.get_data()
-        # print(data)
-        # print(request.files)
-        # if 'file' not in request.files:
-        #     print('No file part')
-        #     return redirect(request.url)
-        # file = request.files['file']
-        # if file.filename == '':
-        #     print('No image selected for uploading')
-        #     return redirect(request.url)
-        # # if file and allowed_file(file.filename):
-        # else:
-        #     filename = file.filename
-        #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #     #print('upload_image filename: ' + filename)
-        #     print('Image successfully uploaded and displayed below')
-        #     # return redirect(request.url)
-        #     # return render_template('upload.html', filename=filename)
-
-        
-        # data = request.get_json()
-        # data = request.get_data()
-        # if data is None:
-        #     print("dooodoo")
-        # print(data)
-        # print(request.files)
-        # #read image file string data
-        # filestr = request.files['file'].read()
         ######################################################################################################################################################
         ######################################################################################################################################################
         ######################################################################################################################################################
@@ -136,7 +105,47 @@ def test():
     else:
         return jsonify(f"Hey!")
 
+# Remove/delete the files in the images and extractedImages folders
+def cleanDirectories():
+    ####################################
+    # Delete the items in subdirectory #
+    ####################################
+    for file_in_sub in os.listdir("./uploadedFile/extractedImages"):
+        if os.path.isdir("./uploadedFile/extractedImages/"+file_in_sub):
+            try:
+                shutil.rmtree("./uploadedFile/extractedImages/"+file_in_sub)
+                # os.rmdir("./images/extractedImages/"+file_in_sub)
+            except OSError as e:
+                print("Error: %s : %s" % ("./uploadedFile/extractedImages/"+file_in_sub, e.strerror))
+        else:
+            try:
+                os.remove("./uploadedFile/extractedImages/"+file_in_sub)
+            except OSError as e:
+                print("Error: %s : %s" % ("./uploadedFile/extractedImages/"+file_in_sub, e.strerror))
 
+    ##############################################
+    # Delete the items in uploadedFile directory #
+    ##############################################
+    for file_in_main in os.listdir("./uploadedFile"):
+        if os.path.isdir("./uploadedFile/"+file_in_main): # item is a directory
+            continue # do not delete
+        elif os.path.isfile("./uploadedFile/"+file_in_main): # item is a file
+            try:
+                os.remove("./uploadedFile/"+file_in_main)
+            except OSError as e:
+                print("Error: %s : %s" % ("./uploadedFile/"+file_in_main, e.strerror))
+
+    ################################################
+    # Delete the items in upscaledImages directory #
+    ################################################
+    for file_in_upscaled in os.listdir("./upscaledImages"):
+        if os.path.isdir("./upscaledImages/"+file_in_upscaled): # item is a directory
+            continue # do not delete
+        elif os.path.isfile("./upscaledImages/"+file_in_upscaled): # item is a file
+            try:
+                os.remove("./upscaledImages/"+file_in_upscaled)
+            except OSError as e:
+                print("Error: %s : %s" % ("./upscaledImages/"+file_in_upscaled, e.strerror))
 
 # Run the server on the local host
 if __name__ == '__main__':
